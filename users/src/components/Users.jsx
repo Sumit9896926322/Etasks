0a6cb5c83a52f8f9b50d4c1376495542bb6cc5da
Sumeet <sumit@bitcs.in>
2021-09-03 00:37:21 +0530

    adding users task

--- users/src/components/Users.jsx
+++ users/src/components/Users.jsx
@@ -5,52 +5,67 @@ const EndPoint = 'https://reqres.in/api/users?page=';
 const Users = ()=>{
     const [page,setPage] = useState(1);
     const [users,setUsers] = useState([]);
-    const [currentPage,setCurrentPage] = useState(1);
+    const [currPage,setCurrentPage] = useState(0);
     const [err,setError] = useState(false);
 
     useEffect(()=>{
-        fetchUsers();
-    },[page]);
+        fetchUsers(page);
+    },[]);
 
-    const fetchUsers =async ()=>{
-        console.log(`${EndPoint}${page}`);
-        if(!err) {
-            const res = await axios.get(`${EndPoint}${page}`);
-            if (res.data.data.length > 0) {
-                setUsers(users.length > 0 ? [...users, res.data.data] : [res.data.data]);
-                setCurrentPage(page);
-            } else {
-                setError(true);
-                setPage(page - 1);
-            }
+    useEffect(()=>{
+        console.log(currPage);
+        if(users[currPage]) {
+            setError(false);
+            setPage(currPage+1);
         }
-        console.log(users);
+    },[currPage]);
+
+    const fetchUsers =async (page)=>{
+            const res = await axios.get(`${EndPoint}${page}`);
+                if (res.data.data.length > 0 && !users[page-1]) {
+                    setUsers(users.length > 0 ? [...users, res.data.data] : [res.data.data]);
+                    setError(false);
+                    setPage(page);
+                } else {
+                    setError(true);
+                }
+            setCurrentPage(page-1);
+
+        // console.log(`${EndPoint}${page}`);
+        // if(!err) {
+        //     const res = await axios.get(`${EndPoint}${page}`);
+        //     if (res.data.data.length > 0) {
+        //         setUsers(users.length > 0 ? [...users, res.data.data] : [res.data.data]);
+        //         setCurrentPage(page);
+        //     } else {
+        //         setError(true);
+        //         setPage(page - 1);
+        //     }
+        // }
+        // console.log(users);
     }
     return(
         <div>
-            {console.log(users,page,currentPage,err)}
+            {console.log(users,page,err,currPage)}
             {
-                err? <h1>Example heading <span className="badge bg-secondary">No data found</span></h1>:<ul className="list-group w-50 m-auto">
+                err? <h1> <span className="badge bg-secondary my-5">No data found</span></h1>:<ul className="list-group w-50 my-5 m-auto">
                     {
-                        users[currentPage-1] && users[currentPage-1].map((u,index)=>
-                            <li className="page-item"><a className="page-link" href="#">{u.first_name}</a></li>
+                        users[currPage] && users[currPage].map((u,index)=>
+                            <li style={{listStyleType:'none'}} className="page-item"><a className="page-link" href="#">{u.first_name}</a></li>
                         )
                     }
                 </ul>
             }
 
             <nav aria-label="..." className="mx-auto my-5 justify-content-center w-25">
-                <ul className="pagination ">
+                <ul className="pagination  justify-content-center">
 
                     {
                         users && users.map((u,index)=>
-                            <li className="page-item"><a className="page-link" href="#" onClick={()=>{
-                                setCurrentPage(index+1);
-                                setError(false);
-                            }}>{index+1}</a></li>
+                            <li className="page-item"><a className="page-link" href="#" onClick={()=>setCurrentPage(index)}>{index+1}</a></li>
                         )
                     }
-                    <li className="page-item" onClick={()=> setPage(page+1)}>
+                    <li className="page-item" onClick={()=> fetchUsers(page+1)}>
                         <a className="page-link" href="#" >Next</a>
                     </li>
                 </ul>
